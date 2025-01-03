7d5e2736078d7edf989b408bb80ab2e931fdd7ef
Baejiu <jiumaker@naver.com>
2025-01-03 16:11:46 +0900

    fix: env config  파일 추가

--- src/app/api/contact/route.jsx
+++ src/app/api/contact/route.jsx
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server';
 import nodemailer from 'nodemailer';
+import '../../../../envConfig.ts';
 
 const email = process.env.REACT_APP_GMAIL_ADDRESS;
 const pass = process.env.REACT_APP_GMAIL_PASS;
